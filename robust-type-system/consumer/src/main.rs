26da6997db98950591a2ea3e7286b937dc18efd1
Nic Biddell <nicbiddell@pm.me>
2020-04-28 23:30:45 -0400

    Adjust ordering

--- robust-type-system/consumer/src/main.rs
+++ robust-type-system/consumer/src/main.rs
@@ -2,22 +2,24 @@ use chooser::{choose_random_color, ChoiceType};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let choice = choose_random_color()?;
+    match choice {
+        ChoiceType::NoChoiceMade => println!("No choice made"),
+        ChoiceType::Choice(c) => println!("{} was chosen!!", c),
+    }
+
+    // Expanded Form
+
     // let choice = choose_random_color();
     // match choice {
     //     Ok(v) => {
     //         match v {
-    //             ChoiceType::NoChoiceMade => println!("No choice made bitches"),
+    //             ChoiceType::NoChoiceMade => println!("No choice made!"),
     //             ChoiceType::Choice(c) => println!("{} was chosen!!", c),
     //         }
     //     }
     //     Err(e) => eprintln!("Error while choosing color: {}", e)
     // }
-
-    let choice = choose_random_color()?;
-    match choice {
-        ChoiceType::NoChoiceMade => println!("No choice made bitches"),
-        ChoiceType::Choice(c) => println!("{} was chosen!!", c),
-    }
 
     Ok(())
 }
